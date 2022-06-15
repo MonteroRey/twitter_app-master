031d508b0aadf341f8b36b0c0063218be22ffe8f
John Doe <johndoe@example.com>
2022-06-15 16:12:33 +0800

    testing login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
     before_save { email.downcase! }
     validates :name, presence: true , length: {maximum: 50}
     ############################# email #############################################
@@ -7,4 +8,9 @@ class User < ApplicationRecord
     uniqueness: true #{case_sensitive: false}
     has_secure_password
     validates :password, presence: true , length: {minimum: 6}
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string,cost: cost)
+    end
 end
