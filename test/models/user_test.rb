53289c6fd569e9ca937a716e929c66b345a1cd15
John Doe <johndoe@example.com>
2022-06-16 18:04:21 +0800

    modify

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,4 +64,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "A"*5
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_nott @user.authenticated?('')
+  end
 end
