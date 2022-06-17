3af14ab95fd2175c198e53b3553fe33f4b71ee13
John Doe <johndoe@example.com>
2022-06-17 17:41:25 +0800

    modify

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,7 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  def is_logged_in? 
-    !session[:user_id].nil?
+  def logged_in_as()user? 
+    session[:user_id] = user.id
   end
 end
