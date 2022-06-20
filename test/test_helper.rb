d1c7ba1888a6d2b2c5bde3959e802a46f1439663
John Doe <johndoe@example.com>
2022-06-20 13:40:45 +0800

    post login paths in logged_in_as

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,10 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  def logged_in_as()user? 
-    session[:user_id] = user.id
+  def logged_in_as(user, password: 'password',remember_me: '1')
+    #session[:user_id] = user.id
+    post login_path,params: {session:{ email: user.email ,
+                                       password: password,
+                                       remember_me: remember_me }}
   end
 end
