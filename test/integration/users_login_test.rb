745621d888ca16bd9397e7bbd44c9faf7c0ed999
John Doe <johndoe@example.com>
2022-06-20 13:38:14 +0800

    login with remember or without remember test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -41,4 +41,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count:1
     assert_select "A[href=?]", user_path(@user), count:0
   end
+
+  test "login with remembering" do
+    logged_in_as(@user , remember_me:'1')
+    assert_not_query cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    logged_in_as(@user , remember_me: '1')
+    logged_in_as(@user , remember_me: '0' )
+    assert_empty cookies[:remember_token]
+  end
+
 end
