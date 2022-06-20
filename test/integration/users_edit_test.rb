5197c26bcb146b4b87ad3d1f0f14ca4db4d6ba5c
John Doe <johndoe@example.com>
2022-06-20 13:38:41 +0800

    successfull edit test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -2,10 +2,14 @@ require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
     def setup
-
+        @user = users(:rey)
     end
     test "unsuccessful edit" do
-        log_in_as(@user)
         get edit_user_path(@user)
+        assert_template 'users/edit'
+        patch use_path(@user), params: { user: {name: "",
+                                         password: "foo",
+                                         password_confirmation: "bar" }}
+        assert_template 'users/edit'
     end
 end
