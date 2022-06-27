d24ef602a86ec230418eb37306ebdf4b7d557180
Rey Montero <rey.montero@kodakollectiv.com>
2022-06-27 17:35:34 +0800

    add seeder for post

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,10 @@ User.create!(name: "admin",
                  activated: true,
                  activated_at: Time.zone.now)
                  
+end 
+ 
+users=User.order(:created_at).take(6)
+50.times do
+    content = Faker::Lorem.sentence(word_count: 5)
+    users.each {|user| user.posts.create!(content: content)}
 end
-
-
