180dd909df61c9bb01a45379428e9a9c765e395e
Rey Montero <rey.montero@kodakollectiv.com>
2022-06-27 17:31:19 +0800

    deny access unless its logged_in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
 #     deny_access unless logged_in?
     
 #   end
+
+    private
+    def logged_in_user 
+      unless logged_in?
+        store_location
+        flash[:danger]="Please log in" 
+        redirect_to login_url
+      end
+    end
 end
