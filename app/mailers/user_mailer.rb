9f278bfc4fafb0c488fed1afaffacce8d7232523
Rey Montero <rey.montero@kodakollectiv.com>
2022-06-27 10:24:58 +0800

    modify

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ApplicationMailer
     byebug
     @user = user
 
-    mail to: user.email , subject: "Account activation"
+    mail to: user.email , subject: "Account Activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,9 +17,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: user.email , subject: "Reset Password"
   end
 end
